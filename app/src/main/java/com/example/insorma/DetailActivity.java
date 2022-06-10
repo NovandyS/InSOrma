24dc78785a09f4b09152deeaa4ab0c581fb3466b
Andy <andinovandi14@gmail.com>
2022-06-10 21:58:39 +0700

    activity_detail update

--- app/src/main/java/com/example/insorma/DetailActivity.java
+++ app/src/main/java/com/example/insorma/DetailActivity.java
@@ -24,13 +24,13 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        tvfurniturename = findViewById(R.id.furniturename);
-        Tvfurnitureprice = findViewById(R.id.furnitureprice);
-        Tvfurniturerating = findViewById(R.id.furniturerating);
-        Tvfurnituredesc = findViewById(R.id.furnituredesc);
-        imageView = findViewById(R.id.imageViewdetail);
+        tvfurniturename = findViewById(R.id.furnitureName);
+        Tvfurnitureprice = findViewById(R.id.furniturePrice);
+        Tvfurniturerating = findViewById(R.id.furnitureRating);
+        Tvfurnituredesc = findViewById(R.id.furnitureDesc);
+        imageView = findViewById(R.id.ivDetail);
         tvfurniturename.setText(furniture.getFurnitureName().toString());
-        Tvfurnitureprice.setText(furniture.getFurniturePrice().toString());
+        Tvfurnitureprice.setText("$ " + furniture.getFurniturePrice().toString());
         Tvfurniturerating.setText(furniture.getFurnitureRating().toString());
         Tvfurnituredesc.setText(furniture.getFurnitureDesc().toString());
         Glide.with(this)
